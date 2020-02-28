e1ef126285711ef480aea88f8055c827bfc0d298
Tran Minh Quang <quangtm210395@gmail.com>
2020-02-28 11:44:43 +0700

    update logic execute sh

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const shell = require('shelljs');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }), bodyParser.json());
 
 app.post('/github-webhook', async (req, res) => {
-  console.log(JSON.stringify(req.body));
+  const { body } = req;
+  console.log('=== start cicd');
+  console.log(`branchs: ${body.ref}`);
+  console.log(`commit before: ${body.before}`);
+  console.log(`commit after: ${body.after}`);
+  console.log(`pusher: ${body.pusher.name}`);
+
+  try {
+    const r = shell.exec(`bash -c ${process.env.DEPLOY_SCRIPT_PATH || '~/deploy.sh'}`);
+    console.log('exec result: ', r);
+  } catch (error) {
+    console.error(error);
+  }
+  console.log('=== end cicd');
+
   res.status(200).json({ success: true });
 });
 
