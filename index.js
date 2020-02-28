a46c140388138339f900a98af2c9e2cf97a23fa5
Tran Minh Quang <quangtm210395@gmail.com>
2020-02-28 15:56:43 +0700

    adding pm2 config

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const shell = require('shelljs');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }), bodyParser.json());
