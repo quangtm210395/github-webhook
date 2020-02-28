0ff8daa6c0b31be69ce48b9d2541191bf1c37e1d
Tran Minh Quang <quangtm210395@gmail.com>
2020-02-28 15:19:02 +0700

    adding pm2 config

---
+++ github.webhook.pm2.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'github_webhook',
+      script: './index.js',
+      watch: true,
+      env: {
+        PORT: 6968,
+        NODE_ENV: 'development',
+        DEPLOY_SCRIPT_PATH: './test.sh',
+      },
+      env_production: {
+        PORT: 6969,
+        NODE_ENV: 'production',
+        DEPLOY_SCRIPT_PATH: '/home/quangtm/cicd-scripts/deploy.sh',
+      },
+    },
+  ],
+};
